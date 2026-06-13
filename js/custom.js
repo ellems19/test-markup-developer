8d21d46c17d6c8ec0e90218e14dbfd8ebc559ef2
ellems19 <61273434+ellems19@users.noreply.github.com>
2026-06-13 15:39:34 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+flatpickr("#my-calendar-from", {
+	wrap: true,
+	dateFormat: "Y-m-d",
+	 monthSelectorType: "static", 
+	 locale: {
+		firstDayOfWeek: 1,
+		weekdays: {
+			shorthand: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"], 
+			longhand: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+			}
+		  },
+	});
+flatpickr("#my-calendar-to", {
+	wrap: true,
+	dateFormat: "Y-m-d",
+	 monthSelectorType: "static", 
+	 locale: {
+		firstDayOfWeek: 1,
+		weekdays: {
+			shorthand: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"], 
+			longhand: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+			}
+		  },
+	});
+
+function grid() {
+document.getElementById('view-select').classList.add('grid-view');
+}
+function list() {
+document.getElementById('view-select').classList.remove('grid-view');
+} 
